2b7450e66999204174584e17b55dac3b62b9ed99
ohu-star <audtj0703@naver.com>
2024-12-07 17:03:30 +0900

    Project setting

---
+++ src/main/java/gcu/smapill_back/SmapillBackApplication.java
@@ -0,0 +1,13 @@
+package gcu.smapill_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmapillBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmapillBackApplication.class, args);
+	}
+
+}
